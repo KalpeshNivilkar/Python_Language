b54118b74f82d3f2a69222d7b5a0fc8300396fe0
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-13 13:29:13 +0530

    add code

--- escapeCharacter.py
+++ escapeCharacter.py
@@ -1,1 +1,6 @@
 print("hey siri i am a good boy \n so what should i do to impressed a girl")
+
+
+
+'''output : hey siri i am a good boy 
+ so what should i do to impressed a girl'''
---
+++ printStatement.py
@@ -0,0 +1,25 @@
+
+# sep = seprators specified how the object separeded
+print("nik", 7, 9 ,sep="\n")
+print("nik", 7, 9 ,sep="_")
+
+'''output : 
+nik
+7
+9
+nik_7_9'''
+
+# end = specified what to print at the end Why end is useful
+
+'''Print on the same line
+
+Format output cleanly
+
+Avoid extra new lines
+
+Useful in loops, patterns, counters'''
+
+print("kalpesh",0,2, end = " ")
+print("amit")
+
+# output: kalpesh 0 2 amit
