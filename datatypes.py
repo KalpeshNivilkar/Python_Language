6c0645d7f0e1c56cf0748bd18a3e1323bad6c496
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-22 14:21:39 +0530

    add new code

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+# datatypes in python 
+
+int = 101           #integer
+string = "kalpesh"
+float = 9.8
+boolean = True
+list = [1,2,3,"adi",8.5]
+set = {"kalpesh","adinath",45,43,22}                            #Stores unique values only
+dict = {"name": "marks","kalpesh":78, "pratik": 97}
+tuple = (10,20,30)
+print(type(int))                                                     #Stores multiple values (not changeable)
