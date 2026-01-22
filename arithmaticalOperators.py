4cb0215cbf9788d61a2e221085befe9601ab3b50
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-22 14:53:23 +0530

    new file

---
+++ arithmaticalOperators.py
@@ -0,0 +1,13 @@
+# arithmetic operators in python
+
+a = 10
+b = 3
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+floor = a // b
+modulus = a % b
+exponent = a ** b
+
+print(sum,sub,mul,div,floor,modulus,exponent)
