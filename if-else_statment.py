d82c371f3aa4e45fb9fea545deb6b0482e04afce
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-27 12:46:22 +0530

    add code

--- if-else_statment.py
+++ if-else_statment.py
@@ -1,4 +1,4 @@
-light = input("light_color : ").lower()
+"""light = input("light_color : ").lower()
 if(light == "red"):
     print("Stop")
 elif(light == "green"):
@@ -6,4 +6,35 @@ elif(light == "green"):
 elif(light == "yellow"):
     print("look")
 else:
-    print("signal is not working")
+    print("signal is not working")"""
+
+
+
+# new code
+"""def signal(light):
+    if light == "red":
+        print("stop")
+    elif light == "green":
+        print("go")
+    elif light == "yellow":
+        print("look")
+    else:
+        print("invalid color")
+
+light = input("Enter the color of signal light : ")
+signal(light)"""
+
+
+def result(marks):
+    if marks < 100 and marks > 90:
+        print("Grade A")
+    elif marks < 90 and marks > 80:
+        print("Grade B")
+    elif marks < 80 and marks > 60:
+        print("Grade C")
+    else:
+        print("You are fail your marks is : ", marks, "\nbetter luck next time :)")
+
+marks = int(input("Enter your marks :"))
+result(marks)
+
