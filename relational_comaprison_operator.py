78dfa0f2709287e3327e479f256b9c8c956f547b
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-27 12:50:38 +0530

    add code

---
+++ relational_comaprison_operator.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+
+print("true" if(a == b) else "false")
+print("true" if(a != b) else "false")
+print("true" if(a <= b) else "false")
+print("true" if(a >= b) else "false")
+print("true" if(a > b) else "false")
+print("true" if(a < b) else "false")
