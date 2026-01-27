5960b51a3a38b791f5e72c6733ea9b97639a9102
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-01-27 12:08:38 +0530

    add new code

---
+++ if-else_statment.py
@@ -0,0 +1,9 @@
+light = input("light_color : ").lower()
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("signal is not working")
