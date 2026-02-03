486ff8283ac979d1538734b63a3767a03763f991
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2026-02-03 16:07:45 +0530

    add new code

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,7 @@
 print("hello world")
+xyz = 5
+print(xyz)
+x = "kalpesh"
+print(x)
+y = 45.2
+print(y)
---
+++ set.py
@@ -0,0 +1,19 @@
+# this is set tutorials 
+
+my_set = set([10,34,56,21])
+your_set = set([100,2000,3000,10])
+
+union = my_set.union(your_set)
+print(union)
+intersects = my_set.intersection(your_set)
+print(intersects)
+
+
+
+# my_set.add(34)
+# my_set.remove(10)
+# my_set.pop()
+# # my_set.clear()
+# print(my_set)
+
+
